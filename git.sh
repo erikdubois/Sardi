964e9b026d6bf659a0369eeef0ac97d8e7223a2b
Erik Dubois <erik.dubois@gmail.com>
2017-01-02 09:00:18 +0100

    Comment :  on 09:00:18 02-01-2017

--- git.sh
+++ git.sh
@@ -1,19 +1,37 @@
 #!/bin/bash
-#======================================================================================
-# 
-# Author  : Erik Dubois at http://www.erikdubois.be
-# License : Distributed under the terms of GNU GPL version 2 or later
-# 
-# AS ALLWAYS, KNOW WHAT YOU ARE DOING.
-#======================================================================================
-
-
-
+#
+##################################################################################################################
+# Written to be used on 64 bits computers
+# Author 	: 	Erik Dubois
+# Website 	: 	http://www.erikdubois.be
+##################################################################################################################
+##################################################################################################################
+#
+#   DO NOT JUST RUN THIS. EXAMINE AND JUDGE. RUN AT YOUR OWN RISK.
+#
+##################################################################################################################
+# Remember : git push origin master --force
+# Remember : git reset --hard <old-commit-id>
+# git push -f <remote-name> <branch-name>
 # Below command will backup everything inside the project folder
-git add . 
-# Committing to the local repository with a message containing the time details
+git add --all .
 
-git commit -m "Get Sardi at Sourceforge"
+# Give a comment to the commit if you want
+echo "####################################"
+echo "Write your commit comment!"
+echo "####################################"
 
-# Push the local snapshot to a remote destination
+read input
+
+# Committing to the local repository with a message containing the time details and commit text
+curtime=$(date +"%T %d-%m-%Y")
+git commit -m "Comment : $input on $curtime"
+
+# Push the local files to github
+
 git push -u origin master
+
+
+echo "################################################################"
+echo "###################    Git Push Done      ######################"
+echo "################################################################"
